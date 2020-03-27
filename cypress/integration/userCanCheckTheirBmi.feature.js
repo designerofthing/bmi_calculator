b33949df4cdcd05f800e4cec2de36bb393af78f5
Steve <designerofthings@gmail.com>
2020-03-27 11:13:04 +0100

    add test for weight & height input & calculate button

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -2,5 +2,8 @@ describe('User can check their Bmi', ()=> {
     it ('User can enter their weight and height',() => {
         cy.visit('http://localhost:3001')
         cy.contains('BMI Calculator')
+        cy.get('#weight').type('90')
+        cy.get('#height').type('186')
+        cy.get('#Calculate').click()
     })
 })
