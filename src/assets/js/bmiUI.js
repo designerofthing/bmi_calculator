384773291d7ab99ef3c2d162ff6415daa70499c1
Steve <designerofthings@gmail.com>
2020-03-27 13:05:09 +0100

    add BMI classification function and message

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -2,6 +2,7 @@ const calculateBMI = () => {
     let weightInput = document.getElementById("weight")
     let heightInput = document.getElementById("height")
     let results = calculateMetricBMI(weightInput.value, heightInput.value)
+    let message = getBMIClassification(results)
     let displayReslutsElement = document.getElementById('results')
-    displayReslutsElement.innerHTML = `<h1> Your BMI value is: ${results}</h1>`
+    displayReslutsElement.innerHTML = `<h1> Your BMI value is: ${results} and you are ${message}</h1>`
 }
