20cc2a69d98cc3d4c48e39910a8e70aa1b887ae8
Steve <designerofthings@gmail.com>
2020-03-27 10:54:46 +0100

    add server test & test for BMI Calculator text

--- cypress/integration/userCanCheckTheirBmi.feature.js
+++ cypress/integration/userCanCheckTheirBmi.feature.js
@@ -1,5 +1,6 @@
 describe('User can check their Bmi', ()=> {
     it ('User can enter their weight and height',() => {
         cy.visit('http://localhost:3001')
+        cy.contains('BMI Calculator')
     })
 })
