ec76c987b671ab9dec7f4b7d49058985c3e87b43
Steve <designerofthings@gmail.com>
2020-03-27 12:54:46 +0100

    test calculator and functionality

--- src/assets/js/bmiUI.js
+++ src/assets/js/bmiUI.js
@@ -1,7 +1,7 @@
-const caculateBMI = () => {
-    let weightInput = document.getElementById("weight");
-    let heightInput = document.getElementById("height");
-    let results = calculateMetricBMI(weightInput.nodeValue, heightInput.value);
-    let displayReslutsElement = document.getElementById('results');
+const calculateBMI = () => {
+    let weightInput = document.getElementById("weight")
+    let heightInput = document.getElementById("height")
+    let results = calculateMetricBMI(weightInput.value, heightInput.value)
+    let displayReslutsElement = document.getElementById('results')
     displayReslutsElement.innerHTML = `<h1> Your BMI value is: ${results}</h1>`
 }
